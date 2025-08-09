02f33e79d436a34bec1435b53fc9e37e58def865
Augusto Diaz <augustosergiodiaz2@hotmail.com>
2025-08-09 20:27:17 +0100

    returning changes

--- app/ui/forms/login.py
+++ app/ui/forms/login.py
@@ -128,7 +128,14 @@ class App(BaseView):
 
     def iniciar_sesion_biometrico(self):
         if self.controller.fingerPrintLogin():
-            self.destroy()
+            user = self.session.get_user()
+            if UserRole.OPERATIVE == user.role:
+                self.view_controller.show_frame("SampleListFrame")
+                return
+            if UserRole.ADMIN == user.role:
+                print("Usuario con rol ADMIN autenticado.")
+                self.view_controller.show_frame("AdminView")
+                return
 
     def mostrar_credenciales(self):
         """Muestra los campos de usuario y contraseña"""
